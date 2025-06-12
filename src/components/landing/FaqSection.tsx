5f820b3b808beeab7bde6be8ee7784ab33d70b08
Felipe Martins <felipejw.FM@gmail.com>
2025-06-12 15:39:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section class="py

--- src/components/landing/FaqSection.tsx
+++ src/components/landing/FaqSection.tsx
@@ -31,7 +31,7 @@ const faqs = [
 
 export function FaqSection() {
   return (
-    <section className="py-16 md:py-24 bg-gray-50">
+    <section className="py-8 md:py-12 bg-gray-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-center items-center mb-12">
           <HelpCircleIcon className="h-10 w-10 text-primary mr-3" />
