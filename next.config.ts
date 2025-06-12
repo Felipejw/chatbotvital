05597a3cbf1c9fb9c28e392f0f6c8ffbfb703a2a
Felipe Martins <felipejw.FM@gmail.com>
2025-06-12 18:56:09 +0000

    Configure o projeto atual para gerar uma build estática exportável (past

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,4 @@
 
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'checkout.tubaroesdabolsa.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'seguro.chatbotvital.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'chatguru.com.br',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
-  allowedDevOrigins: ['https://6000-firebase-studio-1749659974914.cluster-kc2r6y3mtba5mswcmol45orivs.cloudworkstations.dev'],
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
